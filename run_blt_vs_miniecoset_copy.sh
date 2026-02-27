89e3eae918b7bd1f4907f2efae6957f221cb2324
LeoMoehlenkamp <moehlenkamp11@gmail.com>
2026-02-27 10:41:12 +0100

    changed slurm

--- run_blt_vs_miniecoset_copy.sh
+++ run_blt_vs_miniecoset_copy.sh
@@ -2,9 +2,9 @@
 #SBATCH --partition=klab-l40s
 #SBATCH --nodes=1
 #SBATCH -c 12
-#SBATCH --mem=32G
+#SBATCH --mem=16G
 #SBATCH --gres=gpu:1
-#SBATCH --time=24:00:00
+#SBATCH --time=2:00:00
 #SBATCH --job-name=BLT
 #SBATCH --output=logs/%x_%j.out
 #SBATCH --error=logs/%x_%j.err

475a57cda135199b2e1fc7f12898e57ff87494e9
LeoMoehlenkamp <moehlenkamp11@gmail.com>
2026-02-27 11:03:07 +0100

    changed slurm

--- run_blt_vs_miniecoset_copy.sh
+++ run_blt_vs_miniecoset_copy.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-#SBATCH --partition=gpu
+#SBATCH --partition=klab-l40s
 #SBATCH --nodes=1
 #SBATCH -c 12
-#SBATCH --mem=32G
+#SBATCH --mem=16G
 #SBATCH --gres=gpu:1
-#SBATCH --constraint=A100|H100.80gb
 #SBATCH --time=2:00:00
 #SBATCH --job-name=BLT
 #SBATCH --output=logs/%x_%j.out
@@ -32,18 +31,17 @@ echo "Starting training..."
 
 python blt_vs_model/training_code/train_net_copy.py \
     --network blt_vs_bottleneck \
-    --dataset_mode 0 \
-    --dataset miniecoset \
+    --dataset_mode 2 \
     --timesteps 12 \
     --lateral_connections 1 \
     --topdown_connections 1 \
     --skip_connections 0 \
     --bio_unroll 1 \
-    --batch_size 128 \
-    --batch_size_val_test 128 \
+    --batch_size 64 \
+    --batch_size_val_test 64 \
     --n_epochs 5 \
     --learning_rate 7.5e-4 \
-    --num_workers 8
+    --num_workers 4
 
 echo "-------------------------------------"
 echo "Finished: $(date)"

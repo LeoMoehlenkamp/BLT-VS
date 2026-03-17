ed8338bb2299b5b644682032ffc9a0755179917c
LeoMoehlenkamp <moehlenkamp11@gmail.com>
2026-03-17 09:37:30 +0100

    added new exp

--- run_blt_vs_miniecoset_PCA_3.sh
+++ run_blt_vs_miniecoset_PCA_3.sh
@@ -31,7 +31,7 @@ echo "Starting training..."
 
 python blt_vs_model/training_code/train_net_copy_hooks.py \
     --network blt_vs_bottleneck \
-    --bottlenecks "V4->LOC:209" \
+    --bottlenecks "V1->V2:192" \
     --dataset_mode 0 \
     --dataset miniecoset \
     --timesteps 12 \
@@ -41,7 +41,7 @@ python blt_vs_model/training_code/train_net_copy_hooks.py \
     --bio_unroll 1 \
     --batch_size 64 \
     --batch_size_val_test 64 \
-    --n_epochs 30 \
+    --n_epochs 40 \
     --learning_rate 7.5e-4 \
     --num_workers 4
 

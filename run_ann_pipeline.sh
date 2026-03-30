96104598fbc76f92878329cd31d50db41347673d
LeoMoehlenkamp <moehlenkamp11@gmail.com>
2026-03-30 11:26:17 +0200

    changed model

--- run_ann_pipeline.sh
+++ run_ann_pipeline.sh
@@ -9,7 +9,7 @@
 #SBATCH --output=logs/%x_%j.out
 #SBATCH --error=logs/%x_%j.err
 
-MODEL_NAME="blt_vs_bottleneck__miniecoset__ts12__bn-none__20260316_210800"
+MODEL_NAME="blt_vs_bottleneck__miniecoset__ts12__bn-V1V2-360__20260317_114759"
 USE_BEST=1
 BATCH_SIZE=32
 

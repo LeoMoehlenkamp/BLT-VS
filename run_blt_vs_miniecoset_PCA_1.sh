eaa8e39ae3d57c7c03867149b7e1a00268e873fc
LeoMoehlenkamp <moehlenkamp11@gmail.com>
2026-04-14 20:43:14 +0200

    Changed everything so BN on Skip would work

--- run_blt_vs_miniecoset_PCA_1.sh
+++ run_blt_vs_miniecoset_PCA_1.sh
@@ -4,7 +4,7 @@
 #SBATCH -c 12
 #SBATCH --mem=16G
 #SBATCH --gres=gpu:1
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 #SBATCH --job-name=BLT
 #SBATCH --output=logs/%x_%j.out
 #SBATCH --error=logs/%x_%j.err

a2cceac6a1a84ce3869603e91ab4173387dcc66e
LeoMoehlenkamp <moehlenkamp11@gmail.com>
2026-04-20 09:31:05 +0200

    changed all scripts to follow skip logic

--- run_full_analysis_pipeline.sh
+++ run_full_analysis_pipeline.sh
@@ -12,7 +12,7 @@
 # =============================================
 # CONFIGURE HERE
 # =============================================
-MODEL_NAME="blt_vs_bottleneck__miniecoset__ts12__bn-bnall96skip__20260417_124020"
+MODEL_NAME="blt_vs_bottleneck__miniecoset__ts12__bn-bnall64skip__20260416_130242"
 USE_BEST=1
 BATCH_SIZE=32
 PLOT_PANELS=1

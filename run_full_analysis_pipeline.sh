ae08488c06e03fc1b8ec54225ff32ba252bf2238
LeoMoehlenkamp <moehlenkamp11@gmail.com>
2026-04-28 11:20:38 +0200

    changed slurm for full pipeline new model

--- run_full_analysis_pipeline.sh
+++ run_full_analysis_pipeline.sh
@@ -13,7 +13,7 @@
 # =============================================
 # CONFIGURE HERE
 # =============================================
-MODEL_NAME="blt_vs_bottleneck__miniecoset__ts12__bnall96_BU-TD__20260421_120037"
+MODEL_NAME="blt_vs_bottleneck__miniecoset__ts12__bnall96_BU-TD-Skip__20260423_085848"
 USE_BEST=1
 BATCH_SIZE=32
 PLOT_PANELS=1

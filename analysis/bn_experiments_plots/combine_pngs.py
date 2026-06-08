e6bd3c4cd9de5c730d16fbbb6b808532978dd261
LeoMoehlenkamp <moehlenkamp11@gmail.com>
2026-06-08 20:57:29 +0200

    Update model name in analysis pipeline script for miniecoset

--- analysis/bn_experiments_plots/combine_pngs.py
+++ analysis/bn_experiments_plots/combine_pngs.py
@@ -16,9 +16,9 @@ import matplotlib.image as mpimg
 
 PANELS = [
     (r"C:\Users\moehl\Logs\Final\BU\BNnone_BU\2nd_order\blt_vs_bottleneck__miniecoset__ts12__bn-none__20260316_210800_ann_rdms__time_time_spearman__cosine_ranked\all_areas_time_time_spearman.png", "BNnone_BU"),
-    (r"C:\Users\moehl\Logs\Plots_BA\rdm2.png", "BN-V1V2-12"),
-    (r"C:\Users\moehl\Logs\Plots_BA\rdm3.png", "BN-V2V3-8"),
-    (r"C:\Users\moehl\Logs\Plots_BA\rdm4.png", "BN-all-64"),
+    (r"C:\Users\moehl\Logs\Final\BU\BNV1V2_BU\BNV1V2_BU_192\2nd_order\blt_vs_bottleneck__miniecoset__ts12__bn-V1V2-192__20260317_131444_cosine_ranked_spearman\overview.png", "BNV1V2_BU_192"),
+    (r"C:\Users\moehl\Logs\Plots_BA\rdm3.png", "BNV1V2_BU_32"),
+    (r"C:\Users\moehl\Logs\Final\BU\BNV1V2_BU\BNV1V2_BU_12\2nd_order\blt_vs_bottleneck__miniecoset__ts12__bn-V1V2-12__20260321_053846_ann_rdms__time_time_spearman__cosine_ranked\all_areas_time_time_spearman.png", "BNV1V2_BU_12"),
 ]
 
 SAVE_PATH = r"C:\Users\moehl\Logs\Plots_BA\rdm_combined.png"
--- run_full_analysis_pipeline.sh
+++ run_full_analysis_pipeline.sh
@@ -13,7 +13,7 @@
 # =============================================
 # CONFIGURE HERE
 # =============================================
-MODEL_NAME="blt_vs_bottleneck__miniecoset__ts12__bn-V1V2-32__20260318_183415"
+MODEL_NAME="blt_vs_bottleneck__miniecoset__ts12__bn-V1V2-12__20260321_053846"
 USE_BEST=1
 BATCH_SIZE=32
 PLOT_PANELS=1
